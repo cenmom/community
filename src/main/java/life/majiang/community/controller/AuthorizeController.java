629df94e3632cb708e523469f63a023a3fce4c45
cenmom <1370375698@qq.com>
2019-08-17 17:47:16 +0800

    add application h2 mybatis

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -19,6 +19,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -35,9 +37,10 @@ public class AuthorizeController {
     private String redirectUri;
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state
-                           ) {
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request
+    ) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientISecret);
@@ -46,7 +49,13 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = githubProvider.getUser(accessToken);
-        System.out.print(user.getName());
-        return "index";
+        if (user != null) {
+            //登录成功，写入cookies和session
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";  // 重定向
+        } else {
+            //登录失败
+            return "redirect:/";
+        }
     }
 }
